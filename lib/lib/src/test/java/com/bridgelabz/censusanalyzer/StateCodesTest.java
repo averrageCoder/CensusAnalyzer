c097688844e16519b78a3ef6e02e8c0f81a9ca7e
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 02:11:13 +0530

    ADDED: TC2.2 to make sure CSV file is correct

--- lib/lib/src/test/java/com/bridgelabz/censusanalyzer/StateCodesTest.java
+++ lib/lib/src/test/java/com/bridgelabz/censusanalyzer/StateCodesTest.java
@@ -3,10 +3,12 @@ package com.bridgelabz.censusanalyzer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 public class StateCodesTest {
 	
 	private static final String INDIA_STATES_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
+	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 	
 	@Test
     public void givenIndianStatesCSVFileReturnsCorrectRecords() {
@@ -15,6 +17,18 @@ public class StateCodesTest {
             int numOfRecords = censusAnalyser.loadStateCode(INDIA_STATES_CSV_FILE_PATH);
             assertEquals(37, numOfRecords);
         } catch (CensusAnalyserException e) {
+        }
+    }
+	
+	@Test
+    public void givenIndiaStatesData_WithWrongFile_ShouldThrowException() {
+        try {
+        	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadStateCode(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
 	
