9251a56471f1b4443b16a437ae231f76dff1f938
Harsh R Jain <rjainharsh@gmail.com>
2021-09-21 22:53:23 +0530

    ADDED: TC1.2 to make sure CSV file is correct

--- lib/lib/src/test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
+++ lib/lib/src/test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
@@ -2,12 +2,14 @@ package com.bridgelabz.censusanalyzer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.rules.ExpectedException;
+
 import org.junit.jupiter.api.Test;
 
 public class CensusAnalyzerTest {
 
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
-	
+	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -19,5 +21,16 @@ public class CensusAnalyzerTest {
         }
     }
 	
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+        	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
 	
 }
