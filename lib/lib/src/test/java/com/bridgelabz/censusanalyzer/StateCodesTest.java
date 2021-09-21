58c78ff671b3f8ca48fa614c186405dcfac32037
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 02:32:28 +0530

    ADDED: TC2.3 to ensure csv with comma as delimiter

--- lib/lib/src/test/java/com/bridgelabz/censusanalyzer/StateCodesTest.java
+++ lib/lib/src/test/java/com/bridgelabz/censusanalyzer/StateCodesTest.java
@@ -8,8 +8,9 @@ import org.junit.rules.ExpectedException;
 public class StateCodesTest {
 	
 	private static final String INDIA_STATES_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
-	private static final String WRONG_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 	private static final String INDIAN_CENSUS_EMPTY_FILE = "./src/test/resources/EmptyFile.csv";
+	private static final String INDIAN_STATES_CSV_WRONG_DELIMITER = "./src/test/resources/IndiaStateCode_WrongDelimiter.csv";
 	
 	@Test
     public void givenIndianStatesCSVFileReturnsCorrectRecords() {
@@ -28,8 +29,9 @@ public class StateCodesTest {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
             censusAnalyser.loadStateCode(WRONG_CSV_FILE_PATH);
+            assertEquals(true,false);
         } catch (CensusAnalyserException e) {
-            assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+            assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
     }
 	
@@ -37,7 +39,19 @@ public class StateCodesTest {
     public void givenEmptyCsvFile_ShouldReturnCustomExceptionType() {
         try {
         	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
-            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_EMPTY_FILE);
+            int numOfRecords = censusAnalyser.loadStateCode(INDIAN_CENSUS_EMPTY_FILE);
+            assertEquals(true,false);
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+        }
+    }
+	
+	@Test
+    public void givenWrongDelimiter_InIndiaCensusData_ShouldReturnCustomExceptionType() {
+        try {
+        	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadStateCode(INDIAN_STATES_CSV_WRONG_DELIMITER);
+            assertEquals(true,false);
         } catch (CensusAnalyserException e) {
             assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
         }
