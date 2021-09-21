fa4aa54c6547bd25439a4df9a2036d5c85a67ef3
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 01:17:59 +0530

    ADDED: TC1.3 to make sure the csv is iterable

--- lib/lib/src/test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
+++ lib/lib/src/test/java/com/bridgelabz/censusanalyzer/CensusAnalyzerTest.java
@@ -10,6 +10,8 @@ public class CensusAnalyzerTest {
 
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
 	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+	private static final String INDIAN_CENSUS_EMPTY_FILE = "./src/test/resources/EmptyFile.csv";
+	private static final String INDIAN_CENSUS_CSV_WRONG_DELIMITER = "./src/test/resources/IndiaStateCensusDataWrongDelimiter.csv";
 
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
@@ -28,6 +30,16 @@ public class CensusAnalyzerTest {
             ExpectedException exceptionRule = ExpectedException.none();
             exceptionRule.expect(CensusAnalyserException.class);
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+    
+    @Test
+    public void givenEmptyCsvFile_ShouldReturnCustomExceptionType() {
+        try {
+        	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_EMPTY_FILE);
         } catch (CensusAnalyserException e) {
             assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
         }
