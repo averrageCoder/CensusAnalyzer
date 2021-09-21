6d9baf1cb4164cdc8651b3684ff59f7201b98f00
Harsh R Jain <rjainharsh@gmail.com>
2021-09-22 02:16:14 +0530

    MODIFIED: TC2.2 to make sure CSV file is correct

--- lib/lib/src/test/java/com/bridgelabz/censusanalyzer/StateCodesTest.java
+++ lib/lib/src/test/java/com/bridgelabz/censusanalyzer/StateCodesTest.java
@@ -8,7 +8,7 @@ import org.junit.rules.ExpectedException;
 public class StateCodesTest {
 	
 	private static final String INDIA_STATES_CSV_FILE_PATH = "./src/test/resources/IndiaStateCode.csv";
-	private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+	private static final String WRONG_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
 	
 	@Test
     public void givenIndianStatesCSVFileReturnsCorrectRecords() {
@@ -28,8 +28,18 @@ public class StateCodesTest {
             exceptionRule.expect(CensusAnalyserException.class);
             censusAnalyser.loadStateCode(WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
-            assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+            assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
         }
     }
+	
+//	@Test
+//    public void givenEmptyCsvFile_ShouldReturnCustomExceptionType() {
+//        try {
+//        	StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+//            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_EMPTY_FILE);
+//        } catch (CensusAnalyserException e) {
+//            assertEquals(CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES, e.type);
+//        }
+//    }
 	
 }
